6a4e2631a418c3be320d4f0aeb6826df7e7027fa
Ryan Mercadante <ryan.a.mercadante@gmail.com>
2019-11-06 23:22:09 -0500

    add asyncHandler, custom errorHandler

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
 const colors = require('colors')
+const errorHandler = require('./middleware/error')
 const connectDB = require('./config/db')
 
 const PORT = process.env.PORT || 5000
@@ -17,6 +18,7 @@ connectDB()
 // Route files
 const bootcamps = require('./routes/bootcamps')
 
+
 const app = express()
 
 // Body Parser
@@ -29,6 +31,8 @@ if (process.env.NODE_ENV === 'development') {
 
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps)
+
+app.use(errorHandler)
 
 const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold))
 
